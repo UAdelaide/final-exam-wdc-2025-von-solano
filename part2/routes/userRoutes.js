b9dcd303311da9245ebf243f14ef2ce06d1a59b8
von-solano <a1890523@adelaide.edu.au>
2025-06-20 10:41:04 +0000

    [automatic commit] 2025-06-20 10:41:04 UTC 120.88.144.42

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -47,6 +47,9 @@ router.post('/login', async (req, res) => {
       WHERE username = ? AND password_hash = ?
     `, [username, password]);
 
+    console.log("query result: ", rows);
+
+
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
