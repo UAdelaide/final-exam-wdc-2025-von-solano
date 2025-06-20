7000f71a5ed501dccf03b813eda2dcdc1f630ec7
von-solano <a1890523@adelaide.edu.au>
2025-06-20 04:37:33 +0000

    [automatic commit] 2025-06-20 04:37:33 UTC 120.88.144.42

--- part1/app.js
+++ part1/app.js
@@ -83,7 +83,7 @@ let db;
         ('bronnyjames', 'bronny@example.com', 'hashed9', 'walker');
     `);
 
-    await db.execute()
+    await db.execute(`SELECT user_id FROM Users WHERE username = 'alice123'`)
 
     await db.execute(`
         INSERT INTO Dogs (owner_id, name, size)
