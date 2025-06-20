7c909429cfc91abba875c1e367e195067034a78a
von-solano <a1890523@adelaide.edu.au>
2025-06-20 10:34:11 +0000

    [automatic commit] 2025-06-20 10:34:11 UTC 120.88.144.42

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -187,6 +187,7 @@ function login(){
     // Define function to run on response
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
+            const response.
             alert("Welcome "+response.user.username);
         } else if (this.readyState == 4 && this.status >= 400) {
             alert("Login failed");
