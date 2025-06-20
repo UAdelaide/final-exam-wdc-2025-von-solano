76fb62ddc17331edcd57602d87dc98e157fc341d
von-solano <a1890523@adelaide.edu.au>
2025-06-20 10:57:51 +0000

    [automatic commit] 2025-06-20 10:57:51 UTC 120.88.144.42

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
+const session = require('express-session');
 
 // GET all users (for admin/testing)
 router.get('/', async (req, res) => {
