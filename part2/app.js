3e4b5193a3e6cc3505907578e12aaf470b869d8e
von-solano <a1890523@adelaide.edu.au>
2025-06-20 10:57:05 +0000

    [automatic commit] 2025-06-20 10:57:05 UTC 120.88.144.42

--- part2/app.js
+++ part2/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const session = require
+const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
