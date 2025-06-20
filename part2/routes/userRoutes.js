5da61f8f3b7839030700a23fa53f473f66bcfe7d
von-solano <a1890523@adelaide.edu.au>
2025-06-20 11:06:40 +0000

    [automatic commit] 2025-06-20 11:06:40 UTC 120.88.144.42

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -36,7 +36,7 @@ router.get('/me', (req, res) => {
   res.json(req.session.user);
 });
 
-// POST login (dummy version)
+// POST login
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
