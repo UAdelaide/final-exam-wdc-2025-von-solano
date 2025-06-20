2315b6fa6eda57a5933443ee70f55fd26353fe90
von-solano <a1890523@adelaide.edu.au>
2025-06-20 05:01:27 +0000

    [automatic commit] 2025-06-20 05:01:27 UTC 120.88.144.42

--- part1/app.js
+++ part1/app.js
@@ -83,16 +83,16 @@ let db;
         // insert data
         const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
         if (rows[0].count === 0){
-        await db.execute(`
-        INSERT INTO Users (username, email, password_hash, role)
-        VALUES
-        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-        ('lebronjames', 'lebron@example.com', 'hashed23', 'owner'),
-        ('bronnyjames', 'bronny@example.com', 'hashed9', 'walker');
+            await db.execute(`
+                INSERT INTO Users (username, email, password_hash, role)
+                VALUES
+                ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+                ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+                ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+                ('lebronjames', 'lebron@example.com', 'hashed23', 'owner'),
+                ('bronnyjames', 'bronny@example.com', 'hashed9', 'walker');
 
-        `);
+            `);
         }
 
         await db.execute(`
