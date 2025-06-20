fc670a28137da7d1887532f529f2bd2f9fb84819
von-solano <a1890523@adelaide.edu.au>
2025-06-20 04:58:40 +0000

    [automatic commit] 2025-06-20 04:58:40 UTC 120.88.144.42

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -3,16 +3,16 @@ var router = express.Router();
 var mysql = require('mysql2/promise');
 
 // database connection
-const db = {
+const db_configuration = {
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'DogWalkService'
-}
+};
 
 router.get('/dogs', async (req, res, next) => {
     try{
-        const db = await mysql.createConnection(db);
+        const db = await mysql.createConnection(db_configuration);
         const [rows] = await db.execute(`
             SELECT Dogs.name, Dogs.size, Users.username
             FROM Dogs
