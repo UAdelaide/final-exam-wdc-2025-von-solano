bcd521e75eeb393eac72c408ee9a7392ed896316
von-solano <a1890523@adelaide.edu.au>
2025-06-20 04:38:54 +0000

    [automatic commit] 2025-06-20 04:38:54 UTC 120.88.144.42

--- part1/app.js
+++ part1/app.js
@@ -87,6 +87,7 @@ let db;
         INSERT INTO Dogs (owner_id, name, size)
         VALUES
         ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
+        ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
     `);
 
     } catch (err) {
