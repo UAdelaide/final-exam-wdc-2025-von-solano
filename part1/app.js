94ccf16892f3ecf2ab8f6f33c769bcda8f2282dc
von-solano <a1890523@adelaide.edu.au>
2025-06-20 04:43:44 +0000

    [automatic commit] 2025-06-20 04:43:44 UTC 120.88.144.42

--- part1/app.js
+++ part1/app.js
@@ -51,28 +51,27 @@ let db;
                 role ENUM('owner', 'walker') NOT NULL,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );
-
-            CREATE TABLE WalkRequests (
-                request_id INT AUTO_INCREMENT PRIMARY KEY,
-                dog_id INT NOT NULL,
-                requested_time DATETIME NOT NULL,
-                duration_minutes INT NOT NULL,
-                location VARCHAR(255) NOT NULL,
-                status ENUM('open', 'accepted', 'completed', 'cancelled') DEFAULT 'open',
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                FOREIGN KEY (dog_id) REFERENCES Dogs(dog_id)
-            );
     `);
 
     await db.execute(`
-        CREATE TABLE Dogs (
+            CREATE TABLE Dogs (
                 dog_id INT AUTO_INCREMENT PRIMARY KEY,
                 owner_id INT NOT NULL,
                 name VARCHAR(50) NOT NULL,
                 size ENUM('small', 'medium', 'large') NOT NULL,
                 FOREIGN KEY (owner_id) REFERENCES Users(user_id)
             );
-        `)
+        `);
+
+        await db.execute(`
+            CREATE TABLE Dogs (
+                dog_id INT AUTO_INCREMENT PRIMARY KEY,
+                owner_id INT NOT NULL,
+                name VARCHAR(50) NOT NULL,
+                size ENUM('small', 'medium', 'large') NOT NULL,
+                FOREIGN KEY (owner_id) REFERENCES Users(user_id)
+            );
+        `);
 
     // insert data
     await db.execute(`
