3c5fac5d0ee34c99ad2dd132a0884b43f81d22cf
von-solano <a1890523@adelaide.edu.au>
2025-06-20 11:00:18 +0000

    [automatic commit] 2025-06-20 11:00:18 UTC 120.88.144.42

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -50,6 +50,9 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
+    // create user session
+    req.session.user
+
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
