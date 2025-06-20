a510506305e85947c9939eff334f8ad60785261f
von-solano <a1890523@adelaide.edu.au>
2025-06-20 10:50:17 +0000

    [automatic commit] 2025-06-20 10:50:17 UTC 120.88.144.42

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -205,7 +205,6 @@ function login(){
     xmlhttp.open("POST", "/api/users/login", true);
     xmlhttp.setRequestHeader("Content-type", "application/json");
     xmlhttp.send(JSON.stringify(user));
-
 }
 
 function logout(){
