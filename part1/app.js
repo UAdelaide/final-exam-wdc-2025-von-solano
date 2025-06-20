b68b66e59b8c0d3804e52d7f8c33ae7284328db7
von-solano <a1890523@adelaide.edu.au>
2025-06-20 04:44:04 +0000

    [automatic commit] 2025-06-20 04:44:04 UTC 120.88.144.42

--- part1/app.js
+++ part1/app.js
@@ -64,12 +64,15 @@ let db;
         `);
 
         await db.execute(`
-            CREATE TABLE Dogs (
-                dog_id INT AUTO_INCREMENT PRIMARY KEY,
-                owner_id INT NOT NULL,
-                name VARCHAR(50) NOT NULL,
-                size ENUM('small', 'medium', 'large') NOT NULL,
-                FOREIGN KEY (owner_id) REFERENCES Users(user_id)
+            CREATE TABLE WalkRequests (
+                request_id INT AUTO_INCREMENT PRIMARY KEY,
+                dog_id INT NOT NULL,
+                requested_time DATETIME NOT NULL,
+                duration_minutes INT NOT NULL,
+                location VARCHAR(255) NOT NULL,
+                status ENUM('open', 'accepted', 'completed', 'cancelled') DEFAULT 'open',
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (dog_id) REFERENCES Dogs(dog_id)
             );
         `);
 
