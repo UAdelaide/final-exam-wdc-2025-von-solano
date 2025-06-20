2e8b0c920ff8f4c4afcb24d17c852a82018d5d37
von-solano <a1890523@adelaide.edu.au>
2025-06-20 04:57:18 +0000

    [automatic commit] 2025-06-20 04:57:18 UTC 120.88.144.42

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -16,6 +16,7 @@ router.get('/dogs', async (req, res, next) {
         const [rows] = await db.execute(`
             SELECT Dogs.name, Dogs.size, Users.username
             FROM Dogs
+            JOIN Users ON Dogs.owner_id = Users.user_id
         `)
     }
 });
