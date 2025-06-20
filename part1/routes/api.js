666fb11efa017935b6899de823513af063da186c
von-solano <a1890523@adelaide.edu.au>
2025-06-20 05:46:33 +0000

    [automatic commit] 2025-06-20 05:46:33 UTC 120.88.144.42

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -58,8 +58,8 @@ router.get('/walkers/summary', async (req, res, next) => {
             AVG(WalkRatings.rating) AS average_rating,
             COUNT(WHEN WalkRequests.status = 'completed') AS completed_walks
             FROM Users
-            LEFT JOIN WalkRatings ON Users
-
+            LEFT JOIN WalkRatings ON Users.user_id = WalkRatings.walker_id
+            LEFT JOIN WalkRequests ON Users.user_id = WalkRatings.walker_id
 
         `);
         res.json(rows);
