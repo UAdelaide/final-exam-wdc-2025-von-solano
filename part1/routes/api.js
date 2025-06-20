d9fb9d27ad242bc490386d98114ef76e2013689b
von-solano <a1890523@adelaide.edu.au>
2025-06-20 04:54:55 +0000

    [automatic commit] 2025-06-20 04:54:55 UTC 120.88.144.42

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql2/promise');
 
-const db 
+const db = {
+    host: 'localhost',
+    user: 'root',
+    password: ''
+}
 
 router.get('/dogs', function(req, res, next) {
     try{
