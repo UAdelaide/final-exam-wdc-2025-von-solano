99b1aa77a86716cfcaac46cff1bf575f2e68612d
von-solano <a1890523@adelaide.edu.au>
2025-06-20 11:18:37 +0000

    [automatic commit] 2025-06-20 11:18:37 UTC 120.88.144.42

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -61,6 +61,7 @@ router.post('/login', async (req, res) => {
 
 // POST logout
 router.post('/logout', async (req, res) => {
+  // destroy current user session to logout
   req.session.destroy((err) => {
     if(err){
       return res.status(500).json({ message: 'Logout failed' });
