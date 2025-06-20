98467a937bed8a0b011560b74dd74df0d51a4eae
von-solano <a1890523@adelaide.edu.au>
2025-06-20 05:30:06 +0000

    [automatic commit] 2025-06-20 05:30:06 UTC 120.88.144.42

--- part1/app.js
+++ part1/app.js
@@ -81,20 +81,20 @@ let db;
         `);
 
         await db.execute(`
-CREATE TABLE WalkApplications (
-    application_id INT AUTO_INCREMENT PRIMARY KEY,
-    request_id INT NOT NULL,
-    walker_id INT NOT NULL,
-    applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status ENUM('pending', 'accepted', 'rejected') DEFAULT 'pending',
-    FOREIGN KEY (request_id) REFERENCES WalkRequests(request_id),
-    FOREIGN KEY (walker_id) REFERENCES Users(user_id),
-    CONSTRAINT unique_application UNIQUE (request_id, walker_id)
-);
+        CREATE TABLE IF NOT EXISTS WalkApplications (
+            application_id INT AUTO_INCREMENT PRIMARY KEY,
+            request_id INT NOT NULL,
+            walker_id INT NOT NULL,
+            applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            status ENUM('pending', 'accepted', 'rejected') DEFAULT 'pending',
+            FOREIGN KEY (request_id) REFERENCES WalkRequests(request_id),
+            FOREIGN KEY (walker_id) REFERENCES Users(user_id),
+            CONSTRAINT unique_application UNIQUE (request_id, walker_id)
+        );
         `);
 
         await db.execute(`
-        CREATE TABLE WalkRatings (
+        CREATE TABLE IF NOT EXISTS WalkRatings (
             rating_id INT AUTO_INCREMENT PRIMARY KEY,
             request_id INT NOT NULL,
             walker_id INT NOT NULL,
