f4a5b48f280739dae8cda6eba5ed46cf61142ba8
von-solano <a1890523@adelaide.edu.au>
2025-06-20 04:43:24 +0000

    [automatic commit] 2025-06-20 04:43:24 UTC 120.88.144.42

--- part1/app.js
+++ part1/app.js
@@ -52,14 +52,6 @@ let db;
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );
 
-            CREATE TABLE Dogs (
-                dog_id INT AUTO_INCREMENT PRIMARY KEY,
-                owner_id INT NOT NULL,
-                name VARCHAR(50) NOT NULL,
-                size ENUM('small', 'medium', 'large') NOT NULL,
-                FOREIGN KEY (owner_id) REFERENCES Users(user_id)
-            );
-
             CREATE TABLE WalkRequests (
                 request_id INT AUTO_INCREMENT PRIMARY KEY,
                 dog_id INT NOT NULL,
@@ -71,6 +63,16 @@ let db;
                 FOREIGN KEY (dog_id) REFERENCES Dogs(dog_id)
             );
     `);
+
+    await db.execute(`
+        CREATE TABLE Dogs (
+                dog_id INT AUTO_INCREMENT PRIMARY KEY,
+                owner_id INT NOT NULL,
+                name VARCHAR(50) NOT NULL,
+                size ENUM('small', 'medium', 'large') NOT NULL,
+                FOREIGN KEY (owner_id) REFERENCES Users(user_id)
+            );
+        `)
 
     // insert data
     await db.execute(`
