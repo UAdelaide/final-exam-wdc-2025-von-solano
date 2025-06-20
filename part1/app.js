bb84f320f95f17aab6a0c44d9c080f27d2c1ba1e
von-solano <a1890523@adelaide.edu.au>
2025-06-20 04:32:27 +0000

    [automatic commit] 2025-06-20 04:32:27 UTC 120.88.144.42

--- part1/app.js
+++ part1/app.js
@@ -43,22 +43,22 @@ let db;
 
         // Create a table if it doesn't exist
         await db.execute(`
-CREATE TABLE Users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
-    role ENUM('owner', 'walker') NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+            CREATE TABLE Users (
+                user_id INT AUTO_INCREMENT PRIMARY KEY,
+                username VARCHAR(50) UNIQUE NOT NULL,
+                email VARCHAR(100) UNIQUE NOT NULL,
+                password_hash VARCHAR(255) NOT NULL,
+                role ENUM('owner', 'walker') NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
 
-CREATE TABLE Dogs (
-    dog_id INT AUTO_INCREMENT PRIMARY KEY,
-    owner_id INT NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    size ENUM('small', 'medium', 'large') NOT NULL,
-    FOREIGN KEY (owner_id) REFERENCES Users(user_id)
-);
+            CREATE TABLE Dogs (
+                dog_id INT AUTO_INCREMENT PRIMARY KEY,
+                owner_id INT NOT NULL,
+                name VARCHAR(50) NOT NULL,
+                size ENUM('small', 'medium', 'large') NOT NULL,
+                FOREIGN KEY (owner_id) REFERENCES Users(user_id)
+            );
 
 CREATE TABLE WalkRequests (
     request_id INT AUTO_INCREMENT PRIMARY KEY,
