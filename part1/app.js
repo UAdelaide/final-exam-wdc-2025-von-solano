7b5a442e52663f8a5f2c5ed32d9a552ca56ef6f4
von-solano <a1890523@adelaide.edu.au>
2025-06-20 04:51:11 +0000

    [automatic commit] 2025-06-20 04:51:11 UTC 120.88.144.42

--- part1/app.js
+++ part1/app.js
@@ -6,6 +6,8 @@ var mysql = require('mysql2/promise');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
+
 
 var app = express();
 
