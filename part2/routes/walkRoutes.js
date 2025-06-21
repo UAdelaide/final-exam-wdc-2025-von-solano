bc567d6e11bbf055a6f65ec13bd60461dbf51dbd
von-solano <a1890523@adelaide.edu.au>
2025-06-21 03:45:25 +0000

    [automatic commit] 2025-06-21 03:45:25 UTC 120.88.144.42

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -67,7 +67,7 @@ router.get('/owned_dogs', async (req, res) => {
     return res.status(401).json({ error: 'Access Denied' });
   }
 
-    // get owner dogs using current logged in user ow
+    // get owner dogs using current logged in user id
     try {
       const [rows] = await db.query(`
         SELECT dog_id, name FROM Dogs WHERE owner_id = ?
