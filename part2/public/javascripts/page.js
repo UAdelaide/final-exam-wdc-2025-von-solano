e0e49c49ec34337df10132ae1fc047069864ed08
von-solano <a1890523@adelaide.edu.au>
2025-06-21 03:28:07 +0000

    [automatic commit] 2025-06-21 03:28:07 UTC 120.88.144.42

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -202,7 +202,7 @@ function login(){
 
     // Open connection to server & send the post data using a POST request
     // We will cover POST requests in more detail in week 8
-    xmlhttp.open("POST", "/api/users/login", true);
+    xmlhttp.open("POST", "/api/users/login", true); // get POST request for login
     xmlhttp.setRequestHeader("Content-type", "application/json");
     xmlhttp.send(JSON.stringify(user));
 }
